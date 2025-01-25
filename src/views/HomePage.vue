a3e222c2341bdcb74f846e1966c470faa73fc77b
Valerio Di Zio <diziovalerio@gmail.com>
2025-01-25 14:43:16 +0100

    feat: clean and navbar with homepage

---
+++ src/views/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- Home Content -->
+    <div class="container mt-5">
+      <h1>Welcome to VueApp</h1>
+      <p class="lead">This is the home page of your Vue application.</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
+
+<style>
+/* Stile personalizzato opzionale */
+</style>
