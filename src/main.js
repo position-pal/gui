81dfc9c17a5ea417b6b2b7d107e9efd928cde4fe
Valerio Di Zio <diziovalerio@gmail.com>
2025-01-25 14:43:16 +0100

    feat: clean and navbar with homepage

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {
+  createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 const app = createApp(App)
 
