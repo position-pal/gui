89b2fdfd4579c4455497a84a1e35f8318ae445e0
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-02-14 10:23:45 +0100

    feat: init map view

---
+++ src/components/PersonItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="person-item">
+    <div class="avatar"></div>
+    <div class="info">
+      <h3>{{ person.name }}</h3>
+      <p>{{ person.location }} • {{ person.time }}</p>
+    </div>
+    <div class="distance">{{ person.distance }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["person"]
+};
+</script>
+
+<style scoped>
+.person-item {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  border-bottom: 1px solid #ddd;
+}
+.avatar {
+  width: 40px;
+  height: 40px;
+  background-color: gray;
+  border-radius: 50%;
+  margin-right: 10px;
+}
+.info {
+  flex: 1;
+}
+.distance {
+  color: gray;
+}
+</style>
