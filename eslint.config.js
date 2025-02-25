6f29a85abf80a7150661ea592a2ddd204518ed08
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-02-25 21:14:50 +0100

    build: use eslint recomended config

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,17 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   {
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
-
   {
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
-
   js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  skipFormatting,
+  eslintPluginPrettierRecommended,
+  ...pluginVue.configs['flat/recommended'],
 ]
