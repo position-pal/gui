93ec549425d3e5c3c3968698a93a6aa35598dbfd
Luca Tassinari <luca.tassinari.2000@gmail.com>
2025-03-08 10:01:24 +0100

    fix: register firebase also in sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,14 +1,31 @@
 importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/11.4.0/firebase-messaging-compat.js')
 
-messaging.onBackgroundMessage((payload) => {
-  console.log('Received background message ', payload)
-
-  const notificationTitle = payload.notification.title
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icons/notification-icon.png'
+/* Get the Firebase configuration from the cache */
+async function getFirebaseConfig() {
+  const cache = await caches.open('firebase-config-cache');
+  const response = await cache.match('/firebase-config');
+  if (response) {
+    return response.json();
   }
+  return null;
+}
 
-  self.registration.showNotification(notificationTitle, notificationOptions)
-})
+/* Initialize Firebase. */
+getFirebaseConfig().then(config => {
+  if (config) {
+    firebase.initializeApp(config);
+    const messaging = firebase.messaging();
+    messaging.onBackgroundMessage((payload) => {
+      console.log('Received background message ', payload);
+      const notificationTitle = payload.notification.title;
+      const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/icons/notification-icon.png'
+      };
+      self.registration.showNotification(notificationTitle, notificationOptions);
+    });
+  } else {
+    console.error("Firebase configuration not found in cache");
+  }
+});
